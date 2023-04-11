d05c1e049c7c6de25f0dc844fb341735c541d2e3
Aleksandr Dosaev <alexander.dosaev@gmail.com>
2023-04-11 11:00:45 +0400

    fetch_test

--- src/components/product/product-list/index.js
+++ src/components/product/product-list/index.js
@@ -1,27 +1,26 @@
 import { useEffect, useState } from "react";
 
 import { setTotal } from "../../../store/basket";
-import { fetchProduct } from "../../../store/products";
+
 import Product from "../product-card";
 import { useDispatch, useSelector } from "react-redux";
 
 import ProductSearch from "../product-search";
 
 export const ProductList = () => {
-  const data = useSelector((state) => state.products.products);
+  //const data = useSelector((state) => state.products.products);
+  const [data, setData] = useState([]);
   const searchValue = useSelector((state) => state.products.searchValue);
   const dispatch = useDispatch();
   const addedItems = useSelector((state) => state.basket.items);
 
   fetch("https://642bc380d7081590f92918a7.mockapi.io/items")
     .then((response) => response.json())
-    .then((data) => {
-      data.forEach((item) => {
-        item.quantity = 1;
-      });
-      dispatch(fetchProduct(data));
-    });
+    .then((data) => setData(data));
 
+  data.forEach((item) => {
+    item.quantity = 1;
+  });
   useEffect(() => {
     const total = addedItems.reduce(
       (acc, item) => acc + item.quantity * item.price,
